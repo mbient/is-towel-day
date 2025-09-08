d0c2c242a368fd563b336d1b186557c8f4b351f3
mbient <98650834+mbient@users.noreply.github.com>
2025-09-08 19:05:39 +0200

    Refactor: Generalize function and variable names for flexibility
    - Renamed struct from TowelDayResponse to EventResponse
    - Renamed functions: IsTowelDay to IsEventToday, CalcDaysUntil to CalculateDaysUntil, GenMessage to GenerateMessage
    - Updated handler from isTowelDayHandler to eventHandler
    - Simplified message generation logic

--- backend/main.go
+++ backend/main.go
@@ -1,57 +1,70 @@
 package main
 
 import (
-  "encoding/json"
-  "fmt"
-  "net/http"
-  "time"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
 )
 
-type TowelDayResponse struct {
-  IsTowelDay   bool   `json:"is_towel_day"`
-  TowelDay     string `json:"towel_day"`
-  CurrentDate  string `json:"current_date"`
-  DaysUntil    int    `json:"days_until"`
-  Message      string `json:"message"`
+type EventResponse struct {
+	IsEvent     bool   `json:"is_event"`
+	EventDate   string `json:"event_date"`
+	CurrentDate string `json:"current_date"`
+	DaysUntil   int    `json:"days_until"`
+	Message     string `json:"message"`
 }
 
-func IsTowelDay(date1, date2 time.Time) bool {
-	return date1.Month() == date2.Month() && date1.Day() == date2.Day()
+func IsEventToday(currentDate, eventDate time.Time) bool {
+	return currentDate.Month() == eventDate.Month() && currentDate.Day() == eventDate.Day()
 }
 
-func isTowelDayHandler(w http.ResponseWriter, r *http.Request) {
-  currentDate := time.Now().UTC()
-  towelDay := time.Date(currentDate.Year(), time.May, 25, 0, 0, 0, 0, time.UTC)
+func CalculateDaysUntil(currentDate, eventDate time.Time) int {
+	return int(eventDate.Sub(currentDate).Hours() / 24)
+}
 
-  isTowelDay := IsTowelDay(currentDate, towelDay)
+func GenerateMessage(isEvent bool, days int) string {
+	var message string
+	if isEvent {
+		message = "Today is Towel Day! Don't forget to bring your towel."
+	} else {
+		message = fmt.Sprintf("There are %d days until Towel Day.", days)
+	}
+	return message
+}
 
-  var daysUntil int
-  var message string
+func isEventHandler(w http.ResponseWriter, r *http.Request) {
+	currentDate := time.Now().UTC()
+	towelDay := time.Date(currentDate.Year(), time.May, 25, 0, 0, 0, 0, time.UTC)
+	isEvent := IsEventToday(currentDate, towelDay)
 
-  if isTowelDay {
-    daysUntil = 0
-    message = "Today is Towel Day! Don't forget to bring your towel."
-  } else {
-    if currentDate.After(towelDay) {
-      towelDay = towelDay.AddDate(1, 0, 0)
-    }
-    daysUntil = int(towelDay.Sub(currentDate).Hours() / 24)
-    message = fmt.Sprintf("There are %d days until Towel Day.", daysUntil)
-  }
+	var daysUntil int
+	var message string
 
-  response := TowelDayResponse{
-    IsTowelDay:   isTowelDay,
-    TowelDay:     towelDay.Format("January 2"),
-    CurrentDate:  currentDate.Format("January 2"),
-    DaysUntil:    daysUntil,
-    Message:      message,
-  }
+	if isEvent {
+		daysUntil = 0
+		message = GenerateMessage(isEvent, daysUntil)
+	} else {
+		if currentDate.After(towelDay) {
+			towelDay = towelDay.AddDate(1, 0, 0)
+		}
+		daysUntil = CalculateDaysUntil(currentDate, towelDay)
+		message = GenerateMessage(isEvent, daysUntil)
+	}
 
-  w.Header().Set("Content-Type", "application/json")
-  json.NewEncoder(w).Encode(response)
+	response := EventResponse{
+		IsEvent:     isEvent,
+		EventDate:   towelDay.Format("January 2"),
+		CurrentDate: currentDate.Format("January 2"),
+		DaysUntil:   daysUntil,
+		Message:     message,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
 }
 
 func main() {
-  http.HandleFunc("/is-towel-day", isTowelDayHandler)
-  http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/is-towel-day", isEventHandler)
+	http.ListenAndServe(":8080", nil)
 }
